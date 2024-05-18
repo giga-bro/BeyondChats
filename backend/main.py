749ed59d1e166540d16c6f66e2f8add7b5cd630b
Aditya-Sakpal <aditya.as@somaiya.edu>
2024-05-18 13:23:05 +0530

    confirmation

---
+++ backend/main.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI , Request
+from fastapi.middleware.cors import CORSMiddleware
+import requests
+from typing import List , Dict
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+app = FastAPI()
+
+origins=[
+    'http://localhost',
+    'http://localhost:5173',
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_origins=origins,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+API_ENPOINT =os.getenv('API_ENPOINT')
+@app.post("/get_citations",response_model=List[Dict[str,str]])
+def get_citations(request:Request):
+    response = requests.get(API_ENPOINT)
+
+    data = response.json()
+    citations = []
+    for item in data['data']['data']:
+        if 'source' in item:
+            for source in item['source']:
+                if len(source['link']) > 0:
+                    if 'origin' in request.headers:
+                        if request.headers['origin'] == 'http://localhost:5173' :
+                            citations.append(source)
+                    else:
+                        citations.append({'id':source['id'],'link':source['link']})
+    return citations
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)        
+
+
